1cef4f1765a3d9e932bf192da8c1da1360132697
Nicholas Christopher <nwillc@gmail.com>
2015-11-03 21:32:32 -0500

    CacheWriter

---
+++ src/main/java/com/github/nwillc/simplecache/integration/SCacheWriter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.github.nwillc.simplecache.integration;
+
+import javax.cache.Cache;
+import javax.cache.integration.CacheWriter;
+import javax.cache.integration.CacheWriterException;
+import java.util.Collection;
+import java.util.function.Consumer;
+
+public class SCacheWriter<K,V> implements CacheWriter<K,V> {
+	private final Consumer<Object> deleter;
+	private final Consumer<Cache.Entry<? extends K, ? extends V>> writer;
+
+	public SCacheWriter(Consumer<Object> deleter, Consumer<Cache.Entry<? extends K, ? extends V>> writer) {
+		this.deleter = deleter;
+		this.writer = writer;
+	}
+
+	@Override
+	public void delete(Object key) throws CacheWriterException {
+		deleter.accept(key);
+	}
+
+	@Override
+	public void deleteAll(Collection<?> keys) throws CacheWriterException {
+	   keys.stream().forEach(deleter);
+	}
+
+	@Override
+	public void write(Cache.Entry<? extends K, ? extends V> entry) throws CacheWriterException {
+	   writer.accept(entry);
+	}
+
+	@Override
+	public void writeAll(Collection<Cache.Entry<? extends K, ? extends V>> entries) throws CacheWriterException {
+	   entries.stream().forEach(writer);
+	}
+
+}
---
+++ src/test/java/com/github/nwillc/simplecache/integration/SCacheWriterTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2015, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.github.nwillc.simplecache.integration;
+
+import com.github.nwillc.simplecache.SEntry;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.cache.Cache;
+import javax.cache.integration.CacheWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.data.MapEntry.entry;
+
+public class SCacheWriterTest {
+	private Map<Long,String> map = new HashMap<>();
+	private SCacheWriter<Long,String> cacheWriter = new SCacheWriter<>(map::remove,
+			entry -> map.put(entry.getKey(), entry.getValue()));
+
+	@Before
+	public void setUp() throws Exception {
+	  	map.clear();
+		assertThat(cacheWriter).isInstanceOf(CacheWriter.class);
+	}
+
+	@Test
+	public void shouldDelete() throws Exception {
+	  	map.put(0L, "foo");
+		assertThat(map).containsOnly(entry(0L,"foo"));
+		cacheWriter.delete(0L);
+		assertThat(map).doesNotContain(entry(0L, "foo"));
+	}
+
+	@Test
+	public void shouldDeleteAll() throws Exception {
+		map.put(0L, "foo");
+		map.put(1L, "bar");
+		assertThat(map).containsOnly(entry(0L,"foo"),entry(1L,"bar"));
+		List<Long> keys = new ArrayList<>();
+		keys.add(0L);
+		keys.add(1L);
+		cacheWriter.deleteAll(keys);
+		assertThat(map).doesNotContain(entry(0L, "foo"),entry(1L,"bar"));
+	}
+
+	@Test
+	public void shouldWrite() throws Exception {
+	   	assertThat(map).isEmpty();
+		SEntry<Long,String> entry = new SEntry<>(0L, "foo");
+		cacheWriter.write(entry);
+		assertThat(map).containsOnly(entry(0L,"foo"));
+	}
+
+	@Test
+	public void shouldWriteAll() throws Exception {
+		assertThat(map).isEmpty();
+		List<Cache.Entry<Long,String>> entries = new ArrayList<>();
+		entries.add(new SEntry<>(0L, "foo"));
+		entries.add(new SEntry<>(1L, "bar"));
+		cacheWriter.writeAll((Collection)entries);
+		assertThat(map).containsOnly(entry(0L,"foo"),entry(1L,"bar"));
+	}
+}
